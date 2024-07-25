3645756c0ac5b8c6a9b72eb1570468f45876d3a6
Ishy <irtettedzie554@gmail.com>
2024-07-25 19:29:39 +0000

    firebase setup

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:fitness/common/color_extension.dart';
+import 'package:fitness/firebase_options.dart';
 import 'package:fitness/screens/activity/activity_screen.dart';
 import 'package:fitness/screens/goal/goal_screen.dart';
 import 'package:fitness/screens/home/gym_screen.dart';
@@ -12,7 +14,11 @@ import 'package:flutter/material.dart';
 // import 'package:flutter_dotenv/flutter_dotenv.dart';
 // import 'package:provider/provider.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+);
   runApp(const MyApp());
 }
 
